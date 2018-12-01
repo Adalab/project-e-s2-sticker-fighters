9ba3e8b77a9c7fcf3f2c965a4058b0ae512ccc58
carolcesp <carolinacespedes90@gmail.com>
2018-12-01 14:28:14 +0100

    image js correct

--- _src/assets/js/_image.js
+++ _src/assets/js/_image.js
@@ -8,25 +8,17 @@ const uploadBtn = document.querySelector('.fill--btn');
 const fileField = document.querySelector('#img-selector');
 const profileImage = document.querySelector('.image__photo');
 
-uploadBtn.addEventListener('click', function(){
-    alert('hola');
-});
-
 function getImage(e){
-    console.log('getImage');
   var myFile = e.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
 }
-
 function writeImage() {
-    console.log('writeImage');
   profileImage.src= fr.result;
 }
 
 function fakeFileClick() {
-    console.log('fakeFileClick');
- fileField.click(); 
+ fileField.click();
 }
 
 fileField.addEventListener('change', getImage);

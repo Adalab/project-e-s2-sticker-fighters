84dee5ab505d7d1c909e87ffd58f349891a2de42
trizgb <beatrizgomez92@gmail.com>
2018-12-13 19:37:48 +0100

    reset skills left

--- _src/assets/js/_resetButton.js
+++ _src/assets/js/_resetButton.js
@@ -3,43 +3,47 @@
 const resetButton = document.querySelector('.card__button');
 let clear= '';
 
+function colorReset() {
+  card.classList.remove('card__theme-blue', 'card__theme-red', );
+}
+
+function fontReset() {
+  cardWrapper.classList.remove('card__typo-comic', 'card__typo-monserrat', 'card__typo-ubuntu');
+}
+
 function formReset() {
+  //reset de lo que se escribe en el form
   fullName.value = clear;
   position.value = clear;
   telOrigin.value = clear;
   mailOrigin.value = clear;
   linkedinOrigin.value = clear;
   githubOrigin.value = clear;
+  //reset de lo que se pinta en la card
+  fullNameCard.innerHTML = 'Nombre Apellido';
+  positionCard.innerHTML = 'front-end developer';
+  telDest.innerHTML = clear;
+  mailDest.innerHTML = clear;
+  linkedinDest.innerHTML = clear;
+  githubDest.innerHTML = clear;
 }
 
+function imageReset() {
+  profileImage.style.backgroundImage = clear;
+  mini.style.backgroundImage = clear;
+}
+
+
+// function skillsReset() {
+
+// }
+
 function resetCard(){
-  // fullName.value = fullName.setAttribute('placeholder', 'ppermint') ;
-  // fullName.value = clean;
-  // position.value = clean;
-  // telDest.innerHTML = clean ;
-  // mailOrigin.innerHTML = clean;
-  // linkedinOrigin.innerHTML = clean;
-  // fillFormImput.value = setAttribute('placeholder',' Ej: sally-hill@gmail.com');
-  // fillFormImput.innerHTML = 'Ej: sally-hill@gmail.com';
-  formReset()
+  colorReset();
+  fontReset();
+  formReset();
+  imageReset();
+  // skillsReset();
 }
 
 resetButton.addEventListener('click',resetCard);
-
-console.log(resetButton);
-
-// // //Boton Reset de Maria
-// // let labelName= document.querySelector('.form-label--name');
-// let labelPuesto = document.querySelector('.form-label--puesto');
-// let allInputsClean= document.querySelectorAll('.form-input');
-// let infoClean= "";
-// const reset = document.querySelector('.reset');
-// function clean() {
-//   phoneDest.innerHTML=infoClean;
-//   mailDest.innerHTML=infoClean;
-//   linkDest.innerHTML=infoClean;
-//   gitDest.innerHTML=infoClean;
-//   nameCard.innerHTML=labelName.innerHTML;
-//   puestoCard.innerHTML=labelPuesto.innerHTML;
-// }
-// reset.addEventListener('click',clean);

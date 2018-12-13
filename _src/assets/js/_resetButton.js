c8579cdb023d51905c9075ff3c02a159c2145bde
Samanta Noblea <samantanoblea@gmail.com>
2018-12-13 14:01:49 +0100

    failler try

---
+++ _src/assets/js/_resetButton.js
@@ -0,0 +1,30 @@
+// 'use strict';
+
+// const resetButton = document.querySelector('.card__button');
+// let fillFormImput= document.querySelector('.fill--form_imput');
+// let clean= '';
+
+// function resetCard(){
+//   // fullName.value = fullName.setAttribute('placeholder', 'ppermint') ;
+//   mailOrigin.innerHTML = clean;
+//   // fillFormImput.value = setAttribute('placeholder',' Ej: sally-hill@gmail.com');
+//   // fillFormImput.value = ' Ej: sally-hill@gmail.com';
+//   // console.log(fillFormImput.value);
+// }
+// resetButton.addEventListener('click',resetCard);
+
+// // //Boton Reset de Maria
+// // let labelName= document.querySelector('.form-label--name');
+// let labelPuesto = document.querySelector('.form-label--puesto');
+// let allInputsClean= document.querySelectorAll('.form-input');
+// let infoClean= "";
+// const reset = document.querySelector('.reset');
+// function clean() {
+//   phoneDest.innerHTML=infoClean;
+//   mailDest.innerHTML=infoClean;
+//   linkDest.innerHTML=infoClean;
+//   gitDest.innerHTML=infoClean;
+//   nameCard.innerHTML=labelName.innerHTML;
+//   puestoCard.innerHTML=labelPuesto.innerHTML;
+// }
+// reset.addEventListener('click',clean);
